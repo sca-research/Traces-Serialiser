81f84a10414efc46b5cd01f908f3ba34f8250ca1
Scott Egerton <scott.egerton@bristol.ac.uk>
2018-08-22 16:23:46 +0100

    Simplified calculation to determine sample coding

--- src/Traces_Serialiser.hpp
+++ src/Traces_Serialiser.hpp
@@ -156,18 +156,16 @@ private:
         Add_Header(Tag_Number_Of_Traces, p_number_of_traces);
         Add_Header(Tag_Number_Of_Samples_Per_Trace, p_samples_per_trace);
 
-        uint8_t sample_coding;
+        uint8_t sample_coding = p_sample_length;
+
         // If the traces are floating point values, set bit 5 to indicate
-        // this as per the Riscure inspector specification.
+        // this as per the Riscure inspector specification: Table K.2.
+        // Sample coding.
         // TODO: Find out what the sample length should be for floats
         // TODO: Ensure floats are saved correctly.
         if constexpr (std::is_floating_point<T_Traces>::value)
         {
-            sample_coding = p_sample_length | 0b10000;
-        }
-        else
-        {
-            sample_coding = p_sample_length | sizeof(T_Traces);
+            sample_coding |= 0b10000;
         }
 
         Add_Header(Tag_Sample_Coding, sample_coding);
